f4f0bf5b53a1aaec07d8c69256611a3f401d6287
P2wn <racecarboy77@gmail.com>
2026-08-23 12:39:35 -0400

    fix: stabilize CI test runtime

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -7,6 +7,7 @@ dependencies {
     implementation("it.unimi.dsi:fastutil:8.5.18")
 
     testImplementation("org.junit.jupiter:junit-jupiter:5.11.4")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks {
